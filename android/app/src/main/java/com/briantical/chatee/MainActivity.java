9dc5ebf5d44952dd868f1a6040c15ae30b67b928
briantical <lutbrianivan@gmail.com>
2021-11-17 16:59:47 +0300

    splash screen, icons, vector icons

--- android/app/src/main/java/com/briantical/chatee/MainActivity.java
+++ android/app/src/main/java/com/briantical/chatee/MainActivity.java
@@ -1,15 +1,20 @@
 package com.briantical.chatee;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
 
-public class MainActivity extends ReactActivity {
+import org.devio.rn.splashscreen.SplashScreen;
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
+public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "Chatee";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this);
+    super.onCreate(savedInstanceState);
   }
 }
