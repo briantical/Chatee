1623f6ee5e508837a94a7b863dee3b2e56480ad3
briantical <lutbrianivan@gmail.com>
2021-11-17 17:18:40 +0300

    feat: setup folder structure

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export {default as Adapter} from './Adapters';
+export {default as Assets} from './Assets';
+export {default as Commons} from './Commons';
+export {default as Components} from './Components';
+export {default as Constants} from './Constants';
+export {default as Screens} from './Screens';
+export {default as Styles} from './Styles';
